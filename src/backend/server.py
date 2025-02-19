f0f9750acfedf6bc086d41c01dacfbc19c646cfb
Beilong <bt132@duke.edu>
2025-02-19 11:31:50 +0800

    update

--- src/backend/server.py
+++ src/backend/server.py
@@ -7,8 +7,6 @@ sys.path.append(os.path.dirname(os.getcwd()))
 from utils import setup_logger
 logger = setup_logger(__name__)
 
-import time
-
 DEVICE="cuda:4"
 
 logger.info("Inializeing Model, this can take 30mins")
